089a223ab7939a06c0e7f688f2fc5ad0b7a6f0c7
Chidiebere <chidioguejiofor@gmail.com>
2018-05-07 14:56:23 +0100

    Combine all api functions
    The request on the api has been combined and neatly arranged

--- js/books.js
+++ js/books.js
@@ -21,5 +21,33 @@ function deleteBook(id, successCallback,errCallback){
 }
 
 
+
+function postBook() {
+    const url = 'http://fakerestapi.azurewebsites.net/api/Books';
+    let data = {
+        "ID": 2,
+        "Title": "Book 199",
+        "Description": "My Book description is short, Not so much in the book afterall",
+        "PageCount": 10,
+        "Excerpt": "My Book description is short",
+        "PublishDate": "2017-10-20T12:07:26.211333+00:00"
+    }
+
+    let fetchData = {
+        method: 'POST',
+        body: data
+    }
+
+    fetch(url, fetchData) // Call the fetch function passing the url of the API as a parameter
+        .then((resp) => {
+            console.log(resp);
+        })
+        .catch(function (error) {
+            // This is where you run code if the server returns any errors
+            console.log(JSON.stringify(error));
+        });
+}
+
+postBook();
 getBooks((data)=> console.dir(data), (err)=> console.log(err));
 deleteBook(100, (data)=> console.dir(data), (err)=> console.dir(err));
--- js/cover-photos.js
+++ js/cover-photos.js
@@ -24,5 +24,29 @@ function getCoverPhoto(id, successCallback,errCallback){
 
 
 
+function postCoverPhoto() {
+    const url = 'http://fakerestapi.azurewebsites.net/api/CoverPhotos';
+    let data = {
+        "ID": 2,
+        "IDBook": 2,
+        "Url": "https://placeholdit.imgix.net/~text?txtsize=33&txt=Book 2&w=250&h=350"
+      }
+
+    let fetchData = {
+        method: 'POST',
+        body: data
+    }
+
+    fetch(url, fetchData) // Call the fetch function passing the url of the API as a parameter
+    .then((resp) => {
+        console.log(resp);
+    })
+    .catch(function (error) {
+        // This is where you run code if the server returns any errors
+        console.log(JSON.stringify(error));
+    });
+}
+
+postCoverPhoto();
 getCoverPhoto(3, (data)=> console.dir(data), (err)=> console.log(err));
 deleteCoverPhoto(100, (data)=> console.dir(data), (err)=> console.dir(err));
