e7fb4c7caff682d004cddd0ec837e396cf7f69c6
Chidiebere <chidioguejiofor@gmail.com>
2018-05-07 14:37:35 +0100

    Complete functionality of fetch functions file
    The request to the API has been updated

--- js/books.js
+++ js/books.js
@@ -8,33 +8,18 @@ function getBooks(successCallback, errCallback){
     .catch((err)=> errCallback(err));
 }
 
+
 function deleteBook(id, successCallback,errCallback){
     fetch(`http://fakerestapi.azurewebsites.net/api/Books/177`, {
         method: 'DELETE',
     })
     .then((resp)=> {
-        if(resp.status) resp.text();
+        if(resp.ok) return resp.text();
         throw resp.statusText;
     })
     .then((data)=> successCallback(data));
 }
 
 
-function deleteCoverPhoto(id, successCallback,errCallback){
-    fetch(`http://fakerestapi.azurewebsites.net/api/Books/${id}`, {
-        method: 'DELETE',
-    })
-    .then((resp)=>{
-        if(resp.ok ) return resp.json(); 
-        throw err;
-    })
-    .then((data)=> successCallback(data))
-    .catch((err)=> errCallback(err));
-}
-
-
-
 getBooks((data)=> console.dir(data), (err)=> console.log(err));
-deleteBook(100, (data)=> console.dir(data), (err)=> console.dir(err));
-
 deleteBook(100, (data)=> console.dir(data), (err)=> console.dir(err));
---
+++ js/cover-photos.js
@@ -0,0 +1,29 @@
+
+
+function deleteCoverPhoto(id, successCallback,errCallback){
+    fetch(`http://fakerestapi.azurewebsites.net/api/CoverPhotos/${id}`, {
+        method: 'DELETE',
+    })
+    .then((resp)=>{
+        if(resp.ok ) return resp.text(); 
+        throw err;
+    })
+    .then((data)=> successCallback(data))
+    .catch((err)=> errCallback(err));
+}
+
+function getCoverPhoto(id, successCallback,errCallback){
+    fetch(`http://fakerestapi.azurewebsites.net/api/CoverPhotos/${id}`)
+    .then((resp)=> {
+        if(resp.ok) return resp.json(); 
+        throw resp.statusText;
+    })
+    .then((data) => successCallback(data))
+    .catch((err)=> errCallback(err));
+}
+
+
+
+getCoverPhoto(3, (data)=> console.dir(data), (err)=> console.log(err));
+
+deleteCoverPhoto(100, (data)=> console.dir(data), (err)=> console.dir(err));
